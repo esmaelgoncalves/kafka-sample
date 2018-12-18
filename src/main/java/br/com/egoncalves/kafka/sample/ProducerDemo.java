a3127969e613cd27078c1f28082663bb79e45d1e
Esmael Goncalves Pains <esmael.goncalves@hotmail.com>
2018-12-18 17:50:57 -0200

    Initial commit

---
+++ src/main/java/br/com/egoncalves/kafka/sample/ProducerDemo.java
@@ -0,0 +1,33 @@
+package br.com.egoncalves.kafka.sample;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.util.Properties;
+
+public class ProducerDemo {
+
+    public static void main(String[] args){
+
+        String bootstrapServers = "localhost:9092";
+
+        //create Producer properties
+        Properties properties = new Properties();
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        //create the producer;
+        KafkaProducer<String, String> producer = new KafkaProducer<String, String>(properties);
+
+        //create the producer record
+        ProducerRecord<String, String> record = new ProducerRecord<String, String>("sample_topic", "hello world");
+        //send data
+        producer.send(record);
+
+        producer.flush();
+        producer.close();
+    }
+}
